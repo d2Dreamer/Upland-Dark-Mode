5a0b07e8799db2f1bcdc88939e7215b106e174ad
d2Dreamer <41088824+d2Dreamer@users.noreply.github.com>
2024-03-14 19:48:29 +0000

    Add slider for Dark Mode
    Additionally store browser state to remember users preference & some minor menu UI

--- popup.js
+++ popup.js
@@ -1,11 +1,19 @@
 // popup.js
 
 document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('toggleButton');
+    const darkModeToggle = document.getElementById('darkModeToggle');
 
-    toggleButton.addEventListener('click', function () {
+    // Set initial state based on stored value or default to false (off)
+    chrome.storage.sync.get('darkModeEnabled', function (data) {
+        darkModeToggle.checked = data.darkModeEnabled || false;
+    });
+
+    darkModeToggle.addEventListener('change', function () {
+        const darkModeEnabled = darkModeToggle.checked;
+        chrome.storage.sync.set({ 'darkModeEnabled': darkModeEnabled });
+
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleDarkMode' });
+            chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleDarkMode', darkModeEnabled: darkModeEnabled });
         });
     });
 });
